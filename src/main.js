08eb99b6fe9a872b0ffb283f16e1a32fe75e1b34
syy616 <shiyuanyuan616@gmail.com>
2022-04-18 15:36:36 +0800

    15:36

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { Button, Select } from 'element-ui';
 import router from './router'
+
+Vue.use(Button)
+Vue.use(Select)
 
 Vue.config.productionTip = false
 
